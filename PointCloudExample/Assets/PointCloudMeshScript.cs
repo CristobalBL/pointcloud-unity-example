7c745f7bff2ab3d0553b0f105eb2335e04e6854b
Cristobal Barrientos <cbarrientoslow@gmail.com>
2018-07-24 11:27:15 +0200

    Add first commit
    Add camera control for android and unity editor.
    Add shader to manage point cloud.
    Add gameobject to render point cloud.

---
+++ PointCloudExample/Assets/PointCloudMeshScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointCloudMeshScript : MonoBehaviour {
+
+	private Mesh mesh;
+	int numPoints = 60000;
+
+	// Use this for initialization
+	void Start () {
+		mesh = new Mesh();
+
+		GetComponent<MeshFilter>().mesh = mesh;
+		GetComponent<MeshRenderer> ().material = new Material (Shader.Find("Custom/VertexColor"));
+		CreateMesh();
+	}
+	
+	void CreateMesh() {
+		Vector3[] points = new Vector3[numPoints];
+		int[] indecies = new int[numPoints];
+		Color[] colors = new Color[numPoints];
+
+		int max = 10; 
+		int min = -10;
+
+		for(int i=0;i<points.Length;++i) {
+			int x = Random.Range (min, max);
+			int y = Random.Range (min, max);
+			int z = Random.Range (min, max);
+			points[i] = new Vector3(x, y, z);
+			indecies[i] = i;
+
+			float value = (float)1.0 * (((float)y - (float)min) / ((float)max - (float)min));
+
+			colors[i] = new Color(value,value,value,1.0f);
+		}
+
+		mesh.vertices = points;
+		mesh.colors = colors;
+		mesh.SetIndices(indecies, MeshTopology.Points,0);
+
+	}
+}
